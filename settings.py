7b2a0641e55573639bd8f65c2f111d0cc000d82f
omaradrianne <oacbapora@gmail.com>
2025-10-31 04:45:19 -0700

    implement settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """initializing the game's settings."""
+        # Screen settings
+
+        # Setting the backgroud color.
+        # (255, 0, 0) is red
+        # (0, 255, 0) is green
+        # (0, 0, 255) is blue
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+
+    
