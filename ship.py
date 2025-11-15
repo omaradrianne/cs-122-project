8280f84eeca8f7102626ac769e34b9b5d815b0cb
omaradrianne <oacbapora@gmail.com>
2025-11-14 18:16:04 -0800

    limit ship range

--- ship.py
+++ ship.py
@@ -47,13 +47,13 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-        if self.moving_up:
+        if self.moving_up and self.rect.top > 0:
             self.y -= self.settings.ship_speed
-        if self.moving_down:
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.y += self.settings.ship_speed
 
         # Update rect object from self.x and self.y.
